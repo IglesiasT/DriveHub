3283bc5df7999df449f6723848d4ee5337308dd6
GonzaloEbbes <Gebbes@fi.uba.ar>
2021-07-09 20:48:09 -0300

    Funcional con muestra, en Windows

--- generador_carpetas.py
+++ generador_carpetas.py
@@ -10,7 +10,10 @@ import os
 OS = platform.system()
 CADENA = "\ "
 BARRA = CADENA[0]
-
+matcheo = {"Guido_Costa": {"107789_Ebbes": True,
+                           "110389_Puccar": False,
+                           "108923_Iglesias": True},
+           "Leonel_Chaves": {"107876_Ianello": True}}
 
 def generador_ruta(sistema_actual: str, BARRA: str) -> str:
     if sistema_actual == "Windows":
@@ -20,17 +23,27 @@ def generador_ruta(sistema_actual: str, BARRA: str) -> str:
         for i in range(4):
             ruta_escritorio.append(divisiones[i])
         path = BARRA.join(ruta_escritorio)
-        print(path)
+        print(path + BARRA)
+        return path
 
 
 def crear_carpetas(matcheo: dict, ruta_sistema: str) -> None:
+    path = ruta_sistema + BARRA
+    os.mkdir(f"{path}Evaluaciones")
+    path += ("Evaluaciones" + BARRA)
     for docente in matcheo:
-        os.mkdir(f"{ruta_sistema}/{docente}")
+        os.mkdir(f"{path}{docente}")
         for alumno in matcheo[docente]:
             padron_alumno = ""
             for i in range(6):
                 padron_alumno += alumno[i]
-            os.mkdir(f"{ruta_sistema}/{docente}/{padron_alumno}")
+            if matcheo[docente][alumno]:
+                os.mkdir(f"{path}{docente}{BARRA}{padron_alumno}")
 
 
 # url del repo: https://github.com/IglesiasT/TP2.git
+def main() -> None:
+    ruta = generador_ruta(OS, BARRA)
+    crear_carpetas(matcheo, ruta)
+
+main()
