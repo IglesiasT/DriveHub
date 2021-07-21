456c42e31a2568187c194683096b8a67307e9c6b
“IglesiasT” <tiglesias@fi.uba.ar>
2021-07-21 19:16:29 -0300

    Planteo con 2 filtros posibles  y borro pedir_filtro()

--- listar_archivos.py
+++ listar_archivos.py
@@ -7,34 +7,6 @@ DATOS_FILTROS = {
     'Carpetas': 'application/vnd.google-apps.folder'
 }
 
-def pedir_tipo() -> str:
-    """
-    Muestra un menu al usuario y le permite escoger el tipo de archivo por el cual
-    desea filtrar
-    POST: Valida la entrada y devuelve una cadena con la opcion elegida
-    """
-    tipo = ''
-    opcion = None
-
-    while opcion != 4:  #optimizar esto y hacerlo escalable
-        print('Ingresa el tipo de archivos que deseas ver')
-        print('1 - Imagenes\n2 - Videos\n3 - Carpetas\n4 - Volver')
-        opcion = int(input('\nIngresa una opción: '))
-
-        if opcion == 1:
-            tipo = 'Imagenes'
-        
-        if opcion == 2:
-            tipo = 'Videos'
-
-        if opcion == 3:
-            tipo = 'Carpetas'
-        
-        elif opcion != 4:
-            print('Opción inválida, volvé a intentar')
-
-    return tipo
-
 def busqueda_por_filtros(filtros : set) -> None:
     """
     if not filtros (lista vacia) printear todos los archivos
@@ -60,28 +32,25 @@ def busqueda_por_filtros(filtros : set) -> None:
     
 
 def main() -> None:
-    filtros_deseados = []
+    filtro_deseado = None
     opcion = None
 
     print('----- Listado de archivos y carpetas -----')
     
     while opcion != 3:
         print('Elige un filtro, si no deseas ninguno elige Salir')
-        print('1 - Tipo\n2 - Nombre\n3 - Salir')
+        print('1 - Carpetas\n2 - Nombre\n3 - Salir')
         opcion = int(input('\nFiltro deseado: '))
 
         if opcion == 1:
-            tipo = pedir_tipo()
-            if tipo:    #Valido de no agregar la cadena vacía
-                filtros_deseados.append(tipo)
+            filtro_deseado = 'Carpeta'
 
         if opcion == 2:
-            nombre_archivos = input('Ingresa el nombre: ')
-            filtros_deseados.append(nombre_archivos)
+            filtro_deseado = input('Ingresa el nombre: ')
 
         elif opcion != 3:
             print('Opción inválida, volvé a intentar')
 
-    busqueda_por_filtros(set(filtros_deseados)) #set para no tener filtros repetidos
+    busqueda_por_filtros(filtro_deseado)
 
 main()
